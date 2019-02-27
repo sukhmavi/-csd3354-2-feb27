1ef43fc40f9a1f5812fc22384efebe9845d7d9fd
visualstudio <visualstudio@122-PC11>
2019-02-27 09:29:24 -0500

    Add project files.

---
+++ mavi 001/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mavi_001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
