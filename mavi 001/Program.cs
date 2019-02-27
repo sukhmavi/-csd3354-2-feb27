6ff7cc7e76cc62bb1460fc67a144d3ede206e977
visualstudio <visualstudio@122-PC11>
2019-02-27 09:29:51 -0500

    mavi

--- mavi 001/Program.cs
+++ mavi 001/Program.cs
@@ -10,6 +10,17 @@ namespace mavi_001
     {
         static void Main(string[] args)
         {
+            class Program
+        {
+            static void Main(string[] args)
+            {
+                var orderProcessor = new OrderProcessor();
+                var order = new Order { DAtePlaced = DateTime.Now, TotalPrice = 100f };
+                OrderProcessor.Process();
+            }
         }
+    }
+
+}
     }
 }
